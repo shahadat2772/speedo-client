0e0817925e385b1e53a27e7ff2894d253ff15238
shahadat2772 <shahadat2772@gmailcom>
2022-04-29 21:39:04 +0600

    login component is now fucntional

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,17 +1,42 @@
 import React from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { auth } from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
 
 const Login = () => {
   // Navigator
   const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+
+  // Hook to sign in
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+  if (error) {
+    toast.error(error.message, {
+      id: "loginError",
+    });
+  }
 
   // Form Hook
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const email = data.email;
     const password = data.password;
+
+    await signInWithEmailAndPassword(email, password);
   };
+  if (user) {
+    toast.success("Logged in Successfully", {
+      id: "LogInSuccess ",
+    });
+    navigate("/home");
+  }
 
   return (
     <div>
