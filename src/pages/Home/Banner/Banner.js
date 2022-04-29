151ef8df958ffd5138ba5f6118ab3dfbc3302b63
shahadat2772 <shahadat2772@gmailcom>
2022-04-29 10:55:33 +0600

    banner compon added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "./Banner.css";
 
 const Banner = () => {
   return (
     <div>
-      <h2>Thsi si nammer</h2>
+      <div class="bannerImage">
+        <div class="bannerContent">
+          <h1>
+            LET'S BOOM YOUR <br /> BUSINESS TOGETHER
+          </h1>
+          <p>
+            SPEEDO Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Eius, mollitia. Aliquid veniam dicta voluptate vero totam
+            necessitatibus harum at illo.
+          </p>
+          <button>Hire me</button>
+        </div>
+      </div>
     </div>
   );
 };
