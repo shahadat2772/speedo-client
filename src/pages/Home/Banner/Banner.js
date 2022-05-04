4927d4b5ce2099de739f85e6c33cc08b700b8cf1
shahadat2772 <shahadat2772@gmailcom>
2022-05-05 01:47:58 +0600

    banner in home page is now responsive

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -4,7 +4,12 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <div>
-      <div className="bannerImage"></div>
+      <div className="bannerImage">
+        <div className="bannerContent">
+          <p className="bannerText1">UNLOCK</p>
+          <p className="bannerText2">THE POTENTIAL</p>
+        </div>
+      </div>
     </div>
   );
 };
